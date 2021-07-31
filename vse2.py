eb2eea69e1798e1c6c7e91135a1b8b80b5678e05
Jameson Quinn <jameson.quinn@gmail.com>
2021-07-31 13:52:39 -0400

    fix bug

--- vse2.py
+++ vse2.py
@@ -66,7 +66,7 @@ class CsvBatch:
             dw.writerow(r)
         myFile.close()
 
-def oneStepWorker(model, nvot, ncand, ms, r1Media, pickiness, pollingError, r2Media, baseSeed=None, i = 0):
+def oneStepWorker(model, nvot, ncand, ms, pickiness, pollingError, r1Media, r2Media, baseSeed=None, i = 0):
 
     if i>0 and i%10 == 0: print('Iteration:', i)
     if baseSeed is not None:
