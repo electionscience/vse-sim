234eb4efc1e00e15a600cc3ce882bd6f3fa77e9b
marcosb <marcos@vendalism.com>
2021-08-02 22:14:07 -0600

    Add some more interesting IrvPrime examples (with condorcet cycle)

--- methods.py
+++ methods.py
@@ -642,6 +642,10 @@ class IrvPrime(Irv):
         0
         >>> IrvPrime().results([[0,1,2]] * 4 + [[2,1,0]] * 3 + [[1,2,0]] * 2)
         [1, 2, 0]
+        >>> IrvPrime().results([[2,1,0]] * 100 + [[1,0,2]] + [[0,2,1]] * 100)
+        [1, 0, 2]
+        >>> IrvPrime().results([[1,2,0]] * 8 + [[2,0,1]] * 6 + [[0,1,2]] * 5)
+        [0, 1, 2]
         """
         if type(ballots) is not list:
             ballots = list(ballots)
