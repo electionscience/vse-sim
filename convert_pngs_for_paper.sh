f81c56f8ae7a50ad58a35e35968ecd2badab8796
Jameson Quinn <jameson.quinn@gmail.com>
2022-12-19 10:24:15 -0500

    Update convert_pngs_for_paper.sh

--- convert_pngs_for_paper.sh
+++ convert_pngs_for_paper.sh
@@ -3,5 +3,5 @@ svgs="*.svg"
 for svg in $svgs
 do
   echo "Converting $svg"
-  /Applications/Inkscape.app/Contents/MacOS/inkscape --export-background-opacity=0 --export-type=png   --export-filename="pngs/$svg.png" --without-gui "$svg"
+  /Applications/Inkscape.app/Contents/MacOS/inkscape --export-background-opacity=0 --export-type=png   --export-filename="pngs/$svg.png" "$svg"
 done
