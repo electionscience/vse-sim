7625c460a27704198b8896386295c7b5fe3b96b6
Marcus Ogren <marcus.ogre@gmail.com>
2023-01-19 10:20:21 +0200

    Improved the honest strategy for STAR

--- ballotEval.py
+++ ballotEval.py
@@ -2,9 +2,12 @@ from methods import *
 from voterModels import *
 from stratFunctions import *
 
+import collections
+
 def stratStats(model, strat, nvot, ncand, niter, stratArgs={}, numWinners=1, pollingError=0.2,
                 usePolls=True, pickiness=0.7, pollFilter=None):
     bulletCount, totalScore = 0, 0
+    scoreCounts = collections.Counter()
     for i in range(niter):
         electorate = model(nvot, ncand)
         if usePolls:
@@ -15,6 +18,11 @@ def stratStats(model, strat, nvot, ncand, niter, stratArgs={}, numWinners=1, pol
         for ballot in ballots:
             ballotSum = sum(ballot)
             totalScore += ballotSum
+            for score in ballot:
+                scoreCounts[score] += 1
             if ballotSum == max(ballot):
                 bulletCount += 1
-    return bulletCount/(nvot*niter), totalScore/(nvot*ncand*niter)
+    scoreList = [0]*(max(int(s) for s in scoreCounts.keys()) + 1)
+    for score, count in scoreCounts.items():
+        scoreList[int(score)] += count/(nvot*ncand*niter)
+    return bulletCount/(nvot*niter), totalScore/(nvot*ncand*niter), scoreList
